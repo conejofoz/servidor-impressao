60383569cd709a45fcf9428614f6fe66692b687d
conejofoz <conejofoz@gmail.com>
2023-05-06 14:02:48 -0300

    adicionado tempo antes de imprimir

--- appPrinter.js
+++ appPrinter.js
@@ -16,24 +16,30 @@ app.get('/', (req, res) => {
 app.get('/imprimir-pdf', (req, res) => {
   const arquivo = req.query.caminhoDoArquivo;
   const usuario = process.env.USERNAME || 'geral'
-  const caminhoDoArquivo = `C:\\Users\\${usuario}\\Downloads\\'${arquivo}`
+  const caminhoDoArquivo = `C:\\Users\\${usuario}\\Downloads\\${arquivo}`
   const nomeDaImpressora = req.query.nomeDaImpressora;
 
   console.log(caminhoDoArquivo)
 
-  pdfPrinter
-    .print(caminhoDoArquivo, nomeDaImpressora)
-    .then(() => {
-      console.log('pdf enviado para a impressora: ' + nomeDaImpressora + ' ' + caminhoDoArquivo)
-      //res.send('PDF enviado para impressão');
-      res.status(200).json({message: 'PDF enviado para impressão'});
-    })
-    .catch((error) => {
-      console.log('erro', error);
-      //res.status(500).send(error);
-      //return res.status(500).json({message: 'Houve um erro no servidor'});
-      return res.status(500).json({message: error});
-    });
+  setTimeout(() => {
+
+    pdfPrinter
+      .print(caminhoDoArquivo, nomeDaImpressora)
+      .then(() => {
+        console.log('pdf enviado para a impressora: ' + nomeDaImpressora + ' ' + caminhoDoArquivo)
+        //res.send('PDF enviado para impressão');
+        res.status(200).json({message: 'PDF enviado para impressão'});
+      })
+      .catch((error) => {
+        console.log('erro', error);
+        //res.status(500).send(error);
+        //return res.status(500).json({message: 'Houve um erro no servidor'});
+        return res.status(500).json({message: error});
+      });
+      
+  },1000)
+
+
 });
 
 
