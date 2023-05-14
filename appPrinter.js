afdcf719289d8c0659a6cd453ed0f8d5442a00a9
conejofoz <conejofoz@gmail.com>
2023-05-14 20:35:11 -0300

    Impressão simultânea na venda e no pacote

--- appPrinter.js
+++ appPrinter.js
@@ -28,19 +28,81 @@ app.get('/imprimir-pdf', (req, res) => {
       .then(() => {
         console.log('pdf enviado para a impressora: ' + nomeDaImpressora + ' ' + caminhoDoArquivo)
         //res.send('PDF enviado para impressão');
-        res.status(200).json({message: 'PDF enviado para impressão'});
+        res.status(200).json({ message: 'PDF enviado para impressão' });
       })
       .catch((error) => {
         console.log('erro', error);
         //res.status(500).send(error);
         //return res.status(500).json({message: 'Houve um erro no servidor'});
-        return res.status(500).json({message: error});
+        return res.status(500).json({ message: error });
       });
-      
-  },1000)
+
+  }, 1000)
 
 
 });
+
+
+app.get('/imprimir-venda', async (req, res) => {
+  try {
+    const arquivo = req.query.arquivo
+    const usuario = process.env.USERNAME || 'geral'
+    const filePath = `C:\\Users\\${usuario}\\Downloads\\${arquivo}`
+
+    console.log(filePath)
+    console.log(arquivo)
+
+    const printerVenda = 'EPSONAC38EF (L3150 Series)';
+    const printerPacote = 'EPSON TM-T20 ReceiptE4';
+
+    const optionsVenda = {
+      printer: printerVenda,
+      copies: 1
+    }
+
+    const optionsPacote = {
+      printer: printerPacote,
+      copies: 2
+    }
+
+
+
+    await imprimirVenda(filePath, optionsPacote)
+    await imprimirVenda(filePath, optionsVenda)
+
+    return res.status(200).json({
+      error: false,
+      message: 'Impressão OK!',
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: 'Impressão Falhou! ' + error.message
+    })
+
+  }
+})
+
+
+
+
+function imprimirVenda(filePath, options) {
+  return new Promise(function (resolve, reject) {
+    for (let i = 0; i < options.copies; i++) {
+      pdfPrinter.print(filePath, options)
+        .then(() => {
+          console.log('Impressão OK!')
+          return resolve()
+        })
+        .catch(err => {
+          console.clear()
+          console.error('Aconteceu um erro: ', err.message)
+          return reject(err)
+        });
+    }
+  })
+}
+
 
 
 
